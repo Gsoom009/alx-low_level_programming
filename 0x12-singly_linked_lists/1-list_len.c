c03776940be85cbbabbca4050a8b30941f4bc772
Gsoom009 <mohamedgasim9797@gmail.com>
2023-07-27 16:20:37 -0700

    fl

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,20 @@
 #include <stdlib.h>
 #include "lists.h"
-size_t list_len(const list_t *h)
-{
-	return list_len_iterative(h);
-}
 
-size_t list_len_iterative(const list_t *h)
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		n++;
 		h = h->next;
 	}
+	return (n);
+}
