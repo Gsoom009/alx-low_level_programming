d07ab0c911cd30849037b7529f22aba3f64d76b1
Gsoom009 <mohamedgasim9797@gmail.com>
2023-07-27 17:19:28 -0700

    ghl

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_list - prints elemnt of linked list 
+ * print_list - prints the elemnt of linked list 
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
