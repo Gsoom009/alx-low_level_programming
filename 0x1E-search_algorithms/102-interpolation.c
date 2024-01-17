408045b5e69e8b47a8bda16e2f9f56c42472bb74
Gsoom009 <mohamedgasim9797@gmail.com>
2024-01-18 01:17:19 +0300

    gccv1sdeedxsdddasd22w

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,50 +1,39 @@
-#include <math.h>
 #include "search_algos.h"
 
 /**
- * jump_search - search a sorted array for a value using jump search method
- * @array: the array to be searched
- * @size: the size of the array (number of elements)
- * @value: the int to be searched for
+ * interpolation_search - searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * Return: 1st index value is found at or -1 if not present or null array
+ * Return: the first index where value is located, or -1 on failure
  */
-int jump_search(int *array, size_t size, int value)
+
+int interpolation_search(int *array, size_t size, int value)
 {
-	int jump = sqrt(size);
-	int start = 0;
-	int section_end = 0;
+	size_t l, m, r;
 
-	if (array == NULL || size == 0 || array[start] > value)
-		return (-1);
-
-	while (section_end < (int)size)
+	if (array != NULL && size > 0)
 	{
-		if (array[section_end] < value)
+		l = 0;
+		r = size - 1;
+		while (array[l] != array[r])
 		{
-			printf("Value checked array[%d] = [%d]\n", section_end, array[section_end]);
-
-			start = section_end;
-			section_end += jump;
-		}
-		else
-		{
-			break;
+			m = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+			if (m < l || m > r)
+			{
+				printf("Value checked array[%lu] is out of range\n", m);
+				break;
+			}
+			printf("Value checked array[%lu] = [%d]\n", m, array[m]);
+			if (array[m] < value)
+				l = m + 1;
+			else if (array[m] > value)
+				r = m - 1;
+			else
+				return (m);
 		}
 	}
-
-	printf("Value found between indexes [%d] and [%d]\n", start, section_end);
-
-	if (section_end > (int)size - 1)
-		section_end = size - 1;
-
-	while (start <= section_end)
-	{
-		printf("Value checked array[%d] = [%d]\n", start, array[start]);
-		if (array[start] == value)
-			return (start);
-		start++;
-	}
-
 	return (-1);
 }}
